550b4b56cbb2444bedac8fe342964a035f35f3a1
riku <u.riku0615@gmail.com>
2026-09-18 22:54:58 +0900

    SQLAlchemyを導入しDB接続設定を追加

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 spots = []
 
-
+# スポットクラスの生成
 class SpotCreate(BaseModel):
     name: str
     address: str
@@ -13,12 +13,12 @@ class SpotCreate(BaseModel):
     memo: str | None = None
     visited: bool = False
 
-
+# スポット一覧取得
 @app.get("/spots")
 def list_spots():
     return spots
 
-
+# スポット登録
 @app.post("/spots" , status_code=201)
 def create_spot(spot: SpotCreate):
     new_spot = {"id": len(spots) + 1, **spot.model_dump()}
@@ -26,3 +26,14 @@ def create_spot(spot: SpotCreate):
     return new_spot
 
 
+# データベース
+from sqlalchemy import create_engine
+from sqlalchemy import declarative_base, sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./spots.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
