edc010aaf549db75ad2761d3e3fa11c3cf2e5459
riku <u.riku0615@gmail.com>
2026-09-23 17:02:40 +0900

    idの重複インデックスを削除

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from database import Base
 class Spot(Base):
     __tablename__ = "spots"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     address = Column(String, nullable=False)
     category = Column(String, nullable=False)
