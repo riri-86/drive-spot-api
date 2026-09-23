486f0579b4f85d8b4cbe9ba09d4b30b830048b6e
riku <u.riku0615@gmail.com>
2026-09-23 21:37:39 +0900

    スポットの登録と一覧取得をPostgresSQLに接続

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from datetime import datetime
+
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from app.database import SessionLocal
+from app.models import Spot
 
 app = FastAPI()
 
-spots = []
 
 # スポットクラスの生成
 class SpotCreate(BaseModel):
@@ -13,27 +19,39 @@ class SpotCreate(BaseModel):
     memo: str | None = None
     visited: bool = False
 
+
+#スポットクラスの参照
+class SpotRead(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int
+    name: str
+    address: str
+    category: str
+    memo: str | None
+    visited: bool
+    created_at: datetime
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 # スポット一覧取得
-@app.get("/spots")
-def list_spots():
-    return spots
+@app.get("/spots", response_model=list[SpotRead])
+def list_spots(db: Session = Depends(get_db)):
+    return db.scalars(select(Spot)).all()
+
 
 # スポット登録
-@app.post("/spots" , status_code=201)
-def create_spot(spot: SpotCreate):
-    new_spot = {"id": len(spots) + 1, **spot.model_dump()}
-    spots.append(new_spot)
+@app.post("/spots", response_model=SpotRead, status_code=201)
+def create_spot(spot: SpotCreate, db: Session = Depends(get_db)):
+    new_spot = Spot(**spot.model_dump())
+    db.add(new_spot)
+    db.commit()
+    db.refresh(new_spot)
     return new_spot
-
-
-# データベース
-from sqlalchemy import create_engine
-from sqlalchemy import declarative_base, sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./spots.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread":False}
-)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
